b4add276d5b2a42528f57ef880fcfa3f3a001b24
nicogulo <nicogulobelas@gmail.com>
2021-05-21 01:42:22 +0700

    add icon lang and button

--- src/elements/Social/SocialFollow.js
+++ src/elements/Social/SocialFollow.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faYoutube,
   faFacebook,
   faTwitter,
   faInstagram,
-  faLinkedin,
+  faReact,
+  faJs,
 } from '@fortawesome/free-brands-svg-icons';
 
 import './index.scss';
 export default function SocialFollow() {
   return (
     <div class="social-container">
-      <h3>Social Follow</h3>
-      <a
-        href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social"
-      >
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      {/* <h3>Social Follow</h3> */}
+      <a href="https://www.reactjs.org/" className="youtube social">
+        <FontAwesomeIcon icon={faReact} size="1x" />
       </a>
 
-      <a
-        href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social"
-      >
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      <a href="https://www.javascript.com/" className="js lang">
+        <FontAwesomeIcon icon={faJs} size="1x" />
       </a>
       <a
         href="https://www.facebook.com/learnbuildteach/"
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Button from 'elements/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faReact,
+  faJs,
+} from '@fortawesome/free-brands-svg-icons';
+
+import '../elements/Social/index.scss';
 
 export default function MostPicked(props) {
   return (
@@ -15,7 +25,17 @@ export default function MostPicked(props) {
             >
               Saya adalah Mahasiswa semester Akhir dari Institut teknologi PLN
               Jakarta. Saat Ini saya sedang mendalami Frontend Web Developer,
-              dan sudah terbiasa munggunaka HTML, CSS, dan JavaScript.
+              dan sudah terbiasa munggunaka HTML, CSS, dan
+              <Button
+                className="btn-lang px-4 mx-3"
+                hasShadow
+                style={{ borderRadius: 30 }}
+              >
+                JavaScript{' '}
+                <a href="https://www.javascript.com/" className="js lang">
+                  <FontAwesomeIcon icon={faJs} size="2x" />
+                </a>
+              </Button>
             </p>
           </div>
           <div className="col-6 pr-2 text-center" style={{ width: 530 }}>
