4f33dcb0e1727142eaa346354758dda9b30a8335
nicogulo <nicogulobelas@gmail.com>
2021-05-30 01:20:18 +0700

    deleted file not use yet

--- src/utils/formatNumber.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default number => {
-  const formatNumbering = new Intl.NumberFormat("id-ID");
-  return formatNumbering.format(number);
-}
