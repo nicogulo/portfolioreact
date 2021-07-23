2c7173e37566ee23b7be93f85c88fcecb5bfc830
nicogulo <nicogulobelas@gmail.com>
2021-07-23 20:21:42 +0700

    fix footer

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -1,85 +1,54 @@
-import React from "react";
-import Button from "elements/Button";
-import IconText from "parts/IconText";
+import React from 'react';
+// import Button from 'elements/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagram,
+  faTwitter,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import IconText from 'parts/IconText';
 
 export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <div className="row">
+        <div className="row d-flex justify-content-center">
           <div className="col-auto" style={{ width: 350 }}>
             <IconText />
-            <p className="brand-tagline">
-              We kaboom your beauty holiday instantly and memorable.
-            </p>
+            {/* <p className="brand-tagline">Let's Connect.</p> */}
           </div>
           <div className="col-auto mr-5">
-            <h6 className="mt-2">For Beinners</h6>
+            <h6 className="mt-2">Let's Connect</h6>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
-                <Button type="link" href="/register">
-                  New Account
-                </Button>
+                <a href="https://instagram.com/_nanithefvck" target="blank">
+                  <p className="brand-tagline">
+                    <FontAwesomeIcon icon={faInstagram} size="1x" />
+                    _nanithefvck
+                  </p>
+                </a>
               </li>
               <li className="list-group-item">
-                <Button type="link" href="/properties">
-                  Start Booking a Room
-                </Button>
+                <a href="https://twitter.com/_xnfvk" target="blank">
+                  <p className="brand-tagline">
+                    {' '}
+                    <FontAwesomeIcon icon={faTwitter} size="1x" /> _xnfvk
+                  </p>
+                </a>
               </li>
               <li className="list-group-item">
-                <Button type="link" href="/use-payments">
-                  Use Payments
-                </Button>
-              </li>
-            </ul>
-          </div>
-          <div className="col-2 mr-5">
-            <h6 className="mt-2">Explore Us</h6>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <Button type="link" href="/careers">
-                  Our Careers
-                </Button>
-              </li>
-              <li className="list-group-item">
-                <Button type="link" href="/privacy">
-                  Privacy
-                </Button>
-              </li>
-              <li className="list-group-item">
-                <Button type="link" href="/terms">
-                  Terms & Conditions
-                </Button>
-              </li>
-            </ul>
-          </div>
-          <div className="col-3">
-            <h6 className="mt-2">Connect Us</h6>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
-                <Button
-                  isExternal
-                  type="link"
-                  href="mailto:support@staycation.id"
-                >
-                  support@staycation.id
-                </Button>
-              </li>
-              <li className="list-group-item">
-                <Button isExternal type="link" href="tel:+622122081996">
-                  021 - 2208 - 1996
-                </Button>
-              </li>
-              <li className="list-group-item">
-                <span>Staycation, Kemang, Jakarta</span>
+                <a href="https://github.com/nicogulo" target="blank">
+                  <p className="brand-tagline">
+                    {' '}
+                    <FontAwesomeIcon icon={faGithub} size="1x" /> nicogulo
+                  </p>
+                </a>
               </li>
             </ul>
           </div>
         </div>
         <div className="row">
-          <div className="col text-center copyrights">
-            Copyright 2019 • All rights reserved • Staycation
-          </div>
+          <div className="col text-center copyrights">Copyright 2021</div>
         </div>
       </div>
     </footer>
